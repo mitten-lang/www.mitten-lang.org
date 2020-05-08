e58c4d8a2a611ddf27c189a8bd203128a1a70e8e
Nathan Corbyn <me@nathancorbyn.com>
2020-05-08 23:39:29 +0100

    Add notice

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+
+type ButtonProps = {
+  href: string,
+  label: string,
+}
+
+export const Button: FunctionComponent<ButtonProps> = props => {
+  return (
+    <div className="Button">
+      <a href={props.href}>{props.label}</a>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h3 className="App-notice">Nothing here just yet!</h3>
+        <Button href="https://github.com/mitten-lang/" label="View on GitHub" />
       </header>
       <p className="App-copyright">Copyright &copy; The Mitten Programming Language 2020. All Rights Reserved.</p>
     </div>
